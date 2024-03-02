18befcce150c02cacaa3dd53f1896c88d07b5b63
Engbino_KarlJeffer_BSIT422E1 <161761462+karljeffer16@users.noreply.github.com>
2024-03-02 11:59:48 +0800

    Initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PRELIM_E3_Assignment_EngbinoKarlJeffer_BSIT42E1.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
